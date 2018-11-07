2b939c4a0b7b9d56ec120263ad3954dc9a787c10
Luis Giordano <14226200+h4cker39@users.noreply.github.com>
2018-11-07 13:58:00 -0200

    Add files via upload

---
+++ General/mario.java
@@ -0,0 +1,12 @@
+	public int [] arr;
+	   static void plusMinus( int n) {
+		for(int i  = 0 ; i< n  ; i++) {
+			for(int k = n -1 ; k > i ; k-- ) {
+				System.out.print(" ");
+			}
+			for(int j = 0 ; j <= i ;j++) {
+				System.out.print("#");
+			}
+			System.out.print("\n");
+		}
+  }
